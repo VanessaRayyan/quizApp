40ba70eb9586fa4f6600114f81de9261b1ab56ef
Vanessa Rayyan <vanessarayyan@outlook.com>
2020-05-13 12:02:05 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+var currentQuestion = 0;
+let score = 0;
+
+function clickToStart() {
+    $('#presentSurface').on('click', '#start', function (event){
+        event.preventDefault();
+        currentQuestion = 1;
+        score = 0;
+        questionRender();
+    });
+}
+
+function questionScoreUpdate() {
+    $('.questionCounter span').text(currentQuestion);
+    $('.scoreCounter span').text(score);
+}
+
+function startScreenRender() {
+    $('#presentSurface').html($(`  <fieldset>
+        <legend id="welcome">Welcome to the continent quiz!</legend>
+             <div id="startsubmit">
+             <input id="start" type="submit" value="Click here to start">
+             </div>
+        </fieldset>`
+    ));    
+}
+
+
+function questionRender() {
+    let question = questions[currentQuestion-1];
+    questionScoreUpdate();
+    $('#presentSurface').html($(  `<p>${question.question}</p>
+        <fieldset>
+            
+            <div  class="container">
+                <input id="answer_1" name="answers" type="radio" value="${question.answers[0]}">
+                <label for="answer_1">${question.answers[0]}</label>
+            </div>
+            
+            <div  class="container">
+                <input id="answer_2" name="answers" type="radio" value="${question.answers[1]}">
+                <label for="answer_2">${question.answers[1]}</label>
+            </div>
+
+            <div class="container">
+                <input id="answer_3" name="answers" type="radio" value="${question.answers[2]}">
+                <label for="answer_3">${question.answers[2]}</label>
+            </div>
+            
+            <div class="container">
+                <input id="answer_4" name="answers" type="radio" value="${question.answers[3]}">
+                <label for="answer_4">${question.answers[3]}</label>
+            </div>
+            
+            <div>
+                <button id="submit">Submit</button>
+            </div>
+
+        </fieldset>`));
+}
+
+function answerCheck() {
+    $('#presentSurface').on('click', '#submit', function (event){
+        event.preventDefault();
+        let actionButton = $('#submit');
+        if (actionButton.text() == 'Submit'){
+            let answerElement = $("[name='answers']:checked");
+            let chosenAnswer = answerElement.val();
+            console.log(chosenAnswer);
+            if (chosenAnswer == undefined) {
+                alert('Please select an answer');
+                return;
+            }
+            let question = questions[currentQuestion-1];
+            if (chosenAnswer == question.answer){
+                $(answerElement).parent().append('<div class="correctAnswer">That is the correct answer!</div>');
+                score++;  
+            } else {
+                $(answerElement).parent().append('<div class="incorrectAnswer">That is incorrect.<br>The correct answer is ' + question.answer + '</div>');  
+            }
+            actionButton.text('Next >');
+            $("[name='answers']").attr('disabled', true);
+        } else {
+            if (currentQuestion<questions.length){
+                currentQuestion++;
+                questionRender();
+            } else {
+                endScreenRender();
+            }
+            
+        }
+    });
+}
+
+function endScreenRender(){
+    $('#presentSurface').html($(`<p>Your score is ${score}/${questions.length}<br> Great job!<br><br>
+    <input id="start" type="submit" value="Restart quiz"></p>`));
+}
+
+function eventHandler() {
+    startScreenRender();
+    clickToStart();
+    answerCheck();
+}
+
+$(eventHandler);
---
+++ questions.js
@@ -0,0 +1,111 @@
+const questions = [
+    {
+        question: "What continenet is the United States in?",
+        answers: [
+            "Africa",
+            "North America",
+            "Europe",
+            "Asia",
+        ],
+        answer: "North America"
+    },
+
+    {
+        question: "What continenet is Lithuania in?",
+        answers: [
+            "Europe",
+            "South America",
+            "Africa",
+            "Asia",
+        ],
+        answer: "Europe"
+    },
+
+    {
+        question: "What continenet is Lebanon in?",
+        answers: [
+            "Africa",
+            "South America",
+            "Asia",
+            "Europe",
+        ],
+        answer: "Asia"
+    },
+
+    {
+        question: "What continenet is Mauritania in?",
+        answers: [
+            "Africa",
+            "Asia",
+            "South America",
+            "Europe",
+        ],
+        answer: "Africa"
+    },
+
+    {
+        question: "What continenet is Iceland in?",
+        answers: [
+            "Antarctica",
+            "Europe",
+            "North America",
+            "Australia",
+        ],
+        answer: "Europe"
+    },
+
+    {
+        question: "What continenet is Chile in?",
+        answers: [
+            "Asia",
+            "South America",
+            "Africa",
+            "Europe",
+        ],
+        answer: "South America"
+    },
+
+    {
+        question: "What continenet is Brazil in?",
+        answers: [
+            "South America",
+            "Europe",
+            "Africa",
+            "Asia",
+        ],
+        answer: "South America"
+    },
+
+    {
+        question: "What continenet is Malaysia in?",
+        answers: [
+            "South America",
+            "Africa",
+            "Europe",
+            "Asia",
+        ],
+        answer: "Asia"
+    },
+
+    {
+        question: "What continenet is Armenia in?",
+        answers: [
+            "South America",
+            "Asia",
+            "Africa",
+            "Europe",
+        ],
+        answer: "Europe"
+    },
+
+    {
+        question: "What continenet is San Marina in?",
+        answers: [
+            "South America",
+            "Europe",
+            "Asia",
+            "Africa",
+        ],
+        answer: "Europe"
+    },
+]
